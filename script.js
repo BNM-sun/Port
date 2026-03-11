521bf95d31a41b74c498a85dd0510fb372477a86
BNM-sun <banbatho@gmail.com>
2026-03-11 12:24:40 +0200

    Added new content

--- script.js
+++ script.js
@@ -15,6 +15,74 @@ toggleButton.addEventListener('click', function() {
 
         toggleButton.textContent = 'Dark Mode'; 
 
+    } });
+    
+    const texts = [ 
+
+    "Hi, I'm Batho Makgau", 
+
+    "I'm a Equity and Futures Strategist and a Web Developer", 
+
+    "I Build Cool Things like market portfolios and websites", 
+
+    "Welcome to My Portfolio!" 
+
+]; 
+
+ 
+
+let textIndex = 0; 
+
+let charIndex = 0; 
+
+let isDeleting = false; 
+
+const typingElement = document.getElementById('typing-headline'); 
+
+ 
+
+function typeEffect() { 
+
+    const currentText = texts[textIndex]; 
+
+    if (isDeleting) { 
+
+        typingElement.textContent = currentText.substring(0, charIndex - 1); 
+
+        charIndex--; 
+
+    } else { 
+
+        typingElement.textContent = currentText.substring(0, charIndex + 1); 
+
+        charIndex++; 
+
     } 
 
-}); 
+    if (!isDeleting && charIndex === currentText.length) { 
+
+        isDeleting = true; 
+
+        setTimeout(typeEffect, 2000); 
+
+        return; 
+
+    } 
+
+    if (isDeleting && charIndex === 0) { 
+
+        isDeleting = false; 
+
+        textIndex = (textIndex + 1) % texts.length; 
+
+    } 
+
+    const speed = isDeleting ? 50 : 100; 
+
+    setTimeout(typeEffect, speed); 
+
+} 
+
+ 
+
+typeEffect(); 
