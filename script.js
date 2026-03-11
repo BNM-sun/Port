6bc4fc2dcf3e34c5f090692eb8605edcb205b4fc
BNM-sun <banbatho@gmail.com>
2026-03-11 11:36:31 +0200

    Yes

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 console.log("JavaScript is connected");
+const toggleButton = document.getElementById('theme-toggle'); 
 
-const toggleButton = document.getElementById('theme-toggle');
+ 
 
-toggleButton.addEventListener('click', function()
- {document.body.classList.toggle('dark-mode');
-    if (document.body.classList.contains('dark-mode'))
-    {toggle.Button.textContnet = 'Light Mode';}
-else {toggleButton.textContnent = 'Dark Mode';}
-});
+toggleButton.addEventListener('click', function() { 
+
+    document.body.classList.toggle('dark-mode'); 
+
+    if (document.body.classList.contains('dark-mode')) { 
+
+        toggleButton.textContent = 'Light Mode'; 
+
+    } else { 
+
+        toggleButton.textContent = 'Dark Mode'; 
+
+    } 
+
+}); 
