68b5c15796747d4cdb7a02727d3d6d3b971f4c12
BNM-sun <banbatho@gmail.com>
2026-03-11 11:31:32 +0200

    Dark mode added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("JavaScript is connected");
+
+const toggleButton = document.getElementById('theme-toggle');
+
+toggleButton.addEventListener('click', function()
+ {document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode'))
+    {toggle.Button.textContnet = 'Light Mode';}
+else {toggleButton.textContnent = 'Dark Mode';}
+});
